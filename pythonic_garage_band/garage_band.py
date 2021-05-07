7e6dd11c509de0f78c20234f94a0281b90bfac1d
Faisal Abuzaid <faisalabuzaid@outlook.com>
2021-05-07 15:49:44 +0300

    finishthelab

--- pythonic_garage_band/garage_band.py
+++ pythonic_garage_band/garage_band.py
@@ -1,67 +1,59 @@
-
-class Musician:
-    """Parent class to all musicians."""
-
-    members = []
-
-    def __init__(self, role, instrument):
-        self.role = role
-        self.instrument = instrument
-        members.append(self)
+class Musician():
+    def __init__(self, name):
+        self.name = name
 
     def __str__(self):
-        return f'I am a {self.role}'
-
+        return f"My name is {self.name} and I play {self.instrument}"
 
     def __repr__(self):
-        return self.role
-
-    def play_solo(self):
-        return f'{self.role} is playing solo on the {self.instrument}'
-
+        return f"{self.__class__.__name__} instance. Name = {self.name}"
 
     def get_instrument(self):
-        return self.instrument
+        return f"{self.instrument}"
+
+    def play_solo(self):
+        return f"{self.solo}"
 
 
 class Guitarist(Musician):
-    pass
+    def __init__(self, name):
+
+        super().__init__(name)
+        self.instrument = "guitar"
+        self.solo = "face melting guitar solo"
 
 class Bassist(Musician):
-    pass
+    def __init__(self, name):
+
+        super().__init__(name)
+        self.instrument = "bass"
+        self.solo = "bom bom buh bom"
+
 
 class Drummer(Musician):
-    pass
+    def __init__(self, name):
+
+        super().__init__(name)
+        self.instrument = "drums"
+        self.solo = "rattle boom crash"
 
 
 
-class Band:
-
-    all = []
-
+class Band():
     def __init__(self, name, members):
-   
         self.name = name
         self.members = members
-        all.append(self)
-
-
-    def play_solos(self):
-
-        solos = ''
-        for element in self.members:
-            solos += f'{element.play_solo()}\n'
-        return solos[:-1]
-
+        Band.instances.append(self)
 
     def __str__(self):
-        return ("We are the " + self.name)
+        return "This is the Band Class"
 
     def __repr__(self):
-        return ("This is the " + self.name)
+        return f"Name: {self.name}. Members: {self.members}."
 
     @classmethod
     def to_list(cls):
-        return cls.all
+        return cls.instances
 
-    
+    def play_solos(self):
+        return [member.play_solo() for member in self.members]
